f1923bafdb0eaa057ccb457d3366e7e25ee7af2d
Nico Haerringer <n.haerringer@bluewin.ch>
2025-04-29 18:46:18 +0200

    Runtime safe option

--- {{cookiecutter.profile_name}}/grid-jobscript.sh
+++ {{cookiecutter.profile_name}}/grid-jobscript.sh
@@ -7,6 +7,6 @@ set +u
 
 echo "hostname:"
 hostname -f
-ln -s /eos/home-n/niharrin/PhD/Higgs/devel/AutoEFT2Obs/.snakemake/singularity/2899c1dcb897412d079191912bd31eca.simg ${{_CONDOR_SCRATCH_DIR}}/2899c1dcb897412d079191912bd31eca.simg
+ln -s /eos/home-n/niharrin/PhD/Higgs/devel/AutoEFT2Obs/.snakemake/singularity/2899c1dcb897412d079191912bd31eca.simg "$_CONDOR_SCRATCH_DIR/2899c1dcb897412d079191912bd31eca.simg"
 
 {exec_job}
